ffc9bbbc06450362a006ab698f8dff9a1519e822
Sean Skiver <seanskiver@gmail.com>
2017-04-05 23:41:33 +0000

    Fixed navigation

--- views/nav.php
+++ views/nav.php
@@ -10,14 +10,35 @@
     <!--</div>-->
     
     
-    <div id="navbar" class="col-lg-12 col-md-12 col-sm-12 navbar navbar-default">
-        <div id="navbar-container" class="col-lg-6 ">
-            <ul id="nav-list" class="nav navbar-nav">
+    <!--<div id="navbar" class="col-lg-12 col-md-12 col-sm-12 navbar navbar-default">-->
+    <!--    <div id="navbar-container" class="col-lg-6 ">-->
+    <!--        <ul id="nav-list" class="nav navbar-nav">-->
+    <!--            <li class="nav-link" data-link="about"><a href="#about-container">About</a></li>-->
+    <!--            <li class="nav-link" data-link="skill"><a href="#skills">Skills</a></li>-->
+    <!--            <li class="nav-link" data-link="portfolio"><a href="#portolio-container">Portfolio</a></li>-->
+    <!--            <li class="nav-link" data-link="contact"><a href="#contact">Contact</a></li>-->
+    <!--        </ul>            -->
+    <!--    </div>-->
+    <!--</div>-->
+    
+    
+<nav class="navbar navbar-inverse" id="navbar" role="navigation">
+        <div class="navbar-header col-lg-2 col-md-2 col-sm-3" >
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
                 <li class="nav-link" data-link="about"><a href="#about-container">About</a></li>
-                <li class="nav-link" data-link="skill"><a href="#skills">Skill</a></li>
+                <li class="nav-link" data-link="skill"><a href="#skills">Skills</a></li>
                 <li class="nav-link" data-link="portfolio"><a href="#portolio-container">Portfolio</a></li>
                 <li class="nav-link" data-link="contact"><a href="#contact">Contact</a></li>
-            </ul>            
+            </ul>
         </div>
-    </div>
-    
+</nav>
